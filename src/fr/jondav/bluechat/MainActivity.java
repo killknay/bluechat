bd609c4cca65daccb0c342030b1d0712e79962f0
david <david@localhost>
2013-07-06 17:09:43 +0200

    Button Quit

--- src/fr/jondav/bluechat/MainActivity.java
+++ src/fr/jondav/bluechat/MainActivity.java
@@ -2,13 +2,30 @@ package fr.jondav.bluechat;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	LinearLayout layoutGlobal = null;    //< Le layout global de l'activité
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		final Button BtQuitter = (Button) layoutGlobal.findViewById(R.id.bt_Quitter);
+		BtQuitter.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			} 
+		});
+		
 		setContentView(R.layout.activity_main);
 	}
 
